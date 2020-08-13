7eb8fbf499f32b763dc06ab6f49cf912abd8e5f4
zhujunhan <136375002@qq.com>
2020-08-13 23:32:35 +0800

    添加文章样式

---
+++ js/index.js
@@ -0,0 +1,11 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    const $searchList = $(".card-row");
+    for (let i = 1; i < 19; i++) {
+      $searchList.append(template('itemHtml', {index: i}));
+    }
+    $('.materialboxed').materialbox();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
